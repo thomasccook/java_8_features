9580838bc3e657beb852b43e3b8292aeb57f60dc
Thomas Cook <thomascook@Thomass-iMac.hsd1.ut.comcast.net>
2021-04-19 21:17:36 -0600

    Updating after presentation

--- src/topic01_annotation/MyProgram.java
+++ src/topic01_annotation/MyProgram.java
@@ -5,25 +5,20 @@ import topic01_annotation.ThirdPartyLibrary.Ann02;
 
 public class MyProgram {
 
-
-	
-	@Ann01(metadata01 = "Employee", metadata02 = "bbb")
+	@Ann01(metadata01 = "aaa", metadata02 = "bbb")
 	@Ann02(metadata03 = "ccc", metadata04 = "ddd")
 	public class Class01 {
 
-		@Ann01(metadata01 = "id", metadata02 = "fff")
+		@Ann01(metadata01 = "eee", metadata02 = "fff")
 		@Ann02(metadata03 = "ggg", metadata04 = "hhh")
 		public String field01;
 
-		@Ann01(metadata01 = "cmployeed_name", metadata02 = "jjj")
+		@Ann01(metadata01 = "iii", metadata02 = "jjj")
 		@Ann02(metadata03 = "kkk", metadata04 = "lll")
 		public void method01() {
 		}
 	}
 
-	
-	
-	
 	@Ann01
 	@Ann02
 	public class Class02 {
@@ -37,6 +32,4 @@ public class MyProgram {
 		public void method01() {
 		}
 	}
-	
-	
 }
