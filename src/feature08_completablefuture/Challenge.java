14cc5dd503ee7e76ab1f40f8baec3edf47dbed8b
Thomas Cook <thomascook@Thomass-iMac.hsd1.ut.comcast.net>
2021-05-06 21:47:07 -0600

    Feature08 Challenge Answer

--- src/feature08_completablefuture/Challenge.java
+++ src/feature08_completablefuture/Challenge.java
@@ -1,18 +1,161 @@
 package feature08_completablefuture;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class Challenge {
 
-	public void process() throws ExecutionException, InterruptedException {
-	  
-		 
+	private static final String URL = "https://jsonplaceholder.typicode.com/todos/";
+	private static final int CALL_COUNT = 10;
+	
+	////////////////////////////////////////////////////////
+	// Use Completable Future
+	
+	
+	public void useCompletableFuture() throws Exception {
+	 
+		System.out.println("****** Get Title List - Use CompletableFuture ******");
+	
+		prn ("Queue'ing");
+		List<CompletableFuture<String>> listCf = new ArrayList<CompletableFuture<String>>();
+		for (int id=1; id <= CALL_COUNT; id++) {
+			CompletableFuture<String> cf = getCompletableFuture(URL, id, "title");
+			listCf.add(cf);
+		}
+		
+		// Process calls as they arrive
+		prn("Waiting");
+		List<String> listTitle = new ArrayList<String>();
+		for (CompletableFuture<String> cf: listCf) {
+			// *** We wait here ***
+			String title = cf.get();
+			prn("Getting #" + (listCf.indexOf(cf) + 1));
+			listTitle.add(title);
+		}
+		
+		prn("\nTitle List:");
+		listTitle.stream().sorted().forEach(System.out::println);
+		
 	}
 
+	public CompletableFuture<String> getCompletableFuture(String url, int id, String field) throws Exception {				
+		return CompletableFuture
+				.supplyAsync(   () -> { pause((long) (3 + (7 * Math.random())));  return null;})
+				.thenApplyAsync( s -> { prn("Requesting #" + id); return null;})
+				.thenApplyAsync( s -> { return getRemoteData(url, id,  field);});										
+	}
+	
+	
+	////////////////////////////////////////////////////////
+	// java.lang.Thread and ConcurrentHashMap
+	
+	ConcurrentHashMap<String, String> chm = new ConcurrentHashMap<String, String> ();
+	
+	
+	public void useConcurrentHashmap() throws Exception {
+		 
+		System.out.println("****** Get Title List - Use java.lang.Thread and ConcurrentHashMap ******");
+		
+		prn ("Launching Threads");
+		for (int id=1; id <= CALL_COUNT; id++) {
+			(new CallThread(URL, id, "title")).start();
+		}
+		
+		prn("Waiting");
+		int previousSize = 0;
+		while(chm.size() < CALL_COUNT) {
+			if (chm.size() > previousSize) {
+				prn("Received " + chm.size() + " records.");
+				previousSize = chm.size();
+			} 
+			pause(1);
+		}
+		
+		prn("\nTitle List:");
+		chm.keySet().stream().sorted().forEach(System.out::println);
+		
+		
+	}
+	
+	// Insert a new number every 1 seconds.
+	class CallThread extends Thread {
+
+		private String url;
+		private int id;
+		private String field;
+		
+		public CallThread(String url, int id, String field) {
+			this.url = url;
+			this.id = id;
+			this.field = field;
+		}
+		
+		public void run() {
+			pause((long) (3 + (7 * Math.random())));
+			prn("Calling #" + id);
+			String title = getRemoteData(url, id,  field);
+			chm.put(title, title);
+		}
+	}
+	
+	
+	////////////////////////////////////////////////////////
+	// Support
+	
+	// Each call takes 3-10 seconds
+	private String getRemoteData(String url, int id,  String field) {
+		
+		String urlAndParameters = url + id;
+		try {
+			String json = this.makeCall(urlAndParameters);
+			Map<String, String> map = parseJSON(json);
+			return map.get(field);			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		return null;
+
+	}
+	
+	// https://stackoverflow.com/questions/1359689/how-to-send-http-request-in-java
+	private String makeCall(String url) throws Exception {
+		
+		StringBuilder sb = new StringBuilder();
+		
+        URL yahoo = new URL(url);
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+        	sb.append(inputLine + "\n");
+        }
+        in.close();
+                
+        return sb.toString();
+	}
+	
+	// https://stackoverflow.com/questions/443499/convert-json-to-map
+    public static Map<String, String> parseJSON(String json) throws ScriptException {
+        ScriptEngineManager sem = new ScriptEngineManager();
+        ScriptEngine engine = sem.getEngineByName("javascript");
+        String script = "Java.asJSONCompatible(" + json + ")";
+        Object result = engine.eval(script);
+        return (Map) result;
+    }
 	
 	private synchronized static void prn(Object obj) {
 		System.out.print(obj);
@@ -32,10 +175,9 @@ public class Challenge {
 	public static void main(String[] args) {
 		Challenge challenge = new Challenge();
 		try {
-			// blocking call
-			challenge.process();
-		} catch (ExecutionException | InterruptedException e) {
-			// TODO Auto-generated catch block
+			challenge.useCompletableFuture();
+			//challenge.useConcurrentHashmap();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}	
